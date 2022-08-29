29fbb93cf3077773da1721f304918c1ab188cc74
cganbold0724 <cganbold0724@gmail.com>
2022-08-29 14:13:16 -0400

    issue in feature sun has been resp;ved and ready to push to feature sun

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,14 @@ public class Main {
         inception.Sun=1;
         inception.Planet=2;
         inception.Moon=4;
+
         System.out.println("the basic structure is ready");
+
         //developing the feature SUN
         SUN omaro =new SUN();
         omaro.color="pink";
         omaro.name="ferro";
-        omaro.radius=123564654654;
+        omaro.radius=123654654;
         omaro.temperature="15161616516";
     }
 }
