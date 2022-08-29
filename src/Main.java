363fc42e25d93820bca14a979aaaca45e98125db
cganbold0724 <cganbold0724@gmail.com>
2022-08-29 13:29:28 -0400

    Solar system ready to be deployed

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         //write code here
         System.out.println("Hello world!");
+        solarSystem inception=new solarSystem();
+        inception.Sun=1;
+        inception.Planet=2;
+        inception.Moon=4;
+        System.out.println("the basic structure is ready");
     }
 }
